fe20bc0bbaf55b97eefbe8d71aaf1c68135fa7dd
Anthony Lopez-Guerra <anthony.lopez.guerra@jbhunt.com>
2021-08-23 16:02:32 -0500

    added user service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { User } from 'src/assets/code-snippets/User';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'fp-app',
@@ -9,10 +11,13 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private titleService: Title) {}
+  user: User;
+  constructor(private titleService: Title, private userService: UserService) {}
+  
 
   ngOnInit(): void {
-    this.titleService.setTitle("Welcome to FoodPlate")
+    this.titleService.setTitle("Welcome to FoodPlate");
+    this.user = this.userService.getUser();
   }
 
 }
